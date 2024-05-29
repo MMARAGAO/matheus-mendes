d3fef55ea016c2754411c025f0157b0ba67001ac
MMARAGAO <mm.aragao@sempreceub.com>
2024-05-29 17:02:39 -0300

    teste2

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { useInView } from 'react-intersection-observer';
 import Header from "../components/Header";
 import Menu from "../components/Menu";
 import Loadding from "../components/Loadding";
@@ -8,17 +9,20 @@ import Section3 from "../components/Section3";
 import Section4 from "../components/Section4";
 import Footer from "../components/Footer";
 
-
 const Home = () => {
     const [isHovered, setIsHovered] = useState(false);
     const [isMouseClick, setIsMouseClick] = useState(false);
     const [isLoadding, setIsLoadding] = useState(true);
     const [isScroll, setIsScroll] = useState(false);
 
+    const [ref1, inView1] = useInView({ triggerOnce: true });
+    const [ref2, inView2] = useInView({ triggerOnce: true });
+    const [ref3, inView3] = useInView({ triggerOnce: true });
+    const [ref4, inView4] = useInView({ triggerOnce: true });
+
     setTimeout(() => {
         setIsLoadding(false);
     }, 1000);
-
 
     // pegar quando o usuario usar o scroll
     useEffect(() => {
@@ -29,9 +33,7 @@ const Home = () => {
                 setIsScroll(false);
             }
         }
-    }
-        , []);
-
+    }, []);
 
     return (
         <div>
@@ -39,15 +41,14 @@ const Home = () => {
             <Header isHovered={isHovered} setIsHovered={setIsHovered} isMouseClick={isMouseClick} setIsMouseClick={setIsMouseClick} isLoadding={isLoadding} isScroll={isScroll} />
             <Menu isHovered={isHovered} setIsHovered={setIsHovered} isMouseClick={isMouseClick} setIsMouseClick={setIsMouseClick} />
             <div className={`container mx-auto space-y-20 px-4 ${isMouseClick ? 'hidden' : ''} ${isLoadding ? 'hidden' : ''}`}>
-                <Section1 />
-                <Section2 />
-                <Section3 />
-                <Section4 />
+                <div ref={ref1} className={inView1 ? 'opacity-100 transition-opacity duration-1000' : 'opacity-0 transition-opacity duration-1000'}><Section1 /></div>
+                <div ref={ref2} className={inView2 ? 'opacity-100 transition-opacity duration-1000' : 'opacity-0 transition-opacity duration-1000'}><Section2 /></div>
+                <div ref={ref3} className={inView3 ? 'opacity-100 transition-opacity duration-1000' : 'opacity-0 transition-opacity duration-1000'}><Section3 /></div>
+                <div ref={ref4} className={inView4 ? 'opacity-100 transition-opacity duration-1000' : 'opacity-0 transition-opacity duration-1000'}><Section4 /></div>
             </div>
             <div className={`${isMouseClick ? 'hidden' : ''} ${isLoadding ? 'hidden' : ''}`}>
                 <Footer />
             </div>
-
         </div>
     );
 }
